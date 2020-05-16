fa528b64f8f92e69800a475a764385fae4459ce5
xlem <xlem@akkoro.io>
2020-05-16 20:03:48 -0230

    add init stub issue: akkoro/assemblylift#4

---
+++ cli/src/commands/init.rs
@@ -0,0 +1,5 @@
+use clap::ArgMatches;
+
+pub fn init(matches: Option<&ArgMatches>) {
+    unimplemented!("init command")
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,15 +1,29 @@
 use clap::{crate_version, Arg, App, SubCommand};
+use crate::commands::init::init;
+use std::collections::HashMap;
+use crate::commands::CommandFn;
+
+mod commands;
 
 fn main() {
     let app = App::new("asml")
         .version(crate_version!())
         .subcommand(
             App::new("init")
+                .arg(
+                    Arg::with_name("language")
+                        .short("l")
+                        .default_value("rust")
+                        .takes_value(true)
+                )
         );
     let matches = app.get_matches();
 
+    let mut command_map = HashMap::<&str, CommandFn>::new();
+    command_map.insert("init", init);
+
     match matches.subcommand() {
-        ("init", init_matches) => {}
+        (name, matches) => command_map[name](matches),
         _ => {}
     }
 }
