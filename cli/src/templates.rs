413807a6df7c3f720a79b78abc757a053cac8599
Dan <xlem@akkoro.io>
2020-06-20 00:49:03 -0230

    reenable lambda runtime loop

--- cli/src/templates.rs
+++ cli/src/templates.rs
@@ -109,7 +109,7 @@ resource "aws_iam_role" "lambda_iam_role" {
 }
 
 resource "aws_lambda_layer_version" "asml_runtime_layer" {
-  filename   = ""
+  filename   = "bootstrap.zip"
   layer_name = "assemblylift-runtime-{{asml_version}}"
 
   compatible_runtimes = ["provided"]
