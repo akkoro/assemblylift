9f5f37b620595c42e97f967d8274a6dad27eb554
xlem <xlem@akkoro.io>
2020-09-28 01:14:26 -0230

    fix versions

--- cli/src/bom/function.rs
+++ cli/src/bom/function.rs
@@ -20,10 +20,10 @@ crate-type = ["cdylib", "rlib"]
 [dependencies]
 direct-executor = "0.3.0"
 serde_json = "1.0.53"
-asml_core = { version = "0.1.1", package = "assemblylift-core-guest" }
-asml_core_io = { version = "0.1.1", package = "assemblylift-core-io-guest" }
-asml_awslambda = { version = "0.1.0", package = "assemblylift-awslambda-guest" }
-asml_awslambda_iomod = { version = "0.1.1", package = "assemblylift-awslambda-iomod-guest" }
+asml_core = { version = "0.2.0, package = "assemblylift-core-guest" }
+asml_core_io = { version = "0.1.0", package = "assemblylift-core-io-guest" }
+asml_awslambda = { version = "0.2.0", package = "assemblylift-awslambda-guest" }
+
 "#;
 
 static FUNCTION_CARGO_CONFIG: &str = r#"# Generated with assemblylift-cli {{asml_version}}
