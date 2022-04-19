ab874f8cea168704ca8dbc1bf90800c5217e11ce
xlem <xlem@akkoro.io>
2022-04-19 13:15:06 -0230

    refactor launcher & runner using crossbeam

--- providers/generic-docker/src/main.rs
+++ providers/generic-docker/src/main.rs
@@ -5,11 +5,16 @@ use std::fs::File;
 use std::io::{BufReader, Read};
 use std::net::SocketAddr;
 use std::os::unix::fs::PermissionsExt;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use clap::crate_version;
+use crossbeam_channel::bounded;
+use crossbeam_utils::atomic::AtomicCell;
+use crossbeam_utils::thread;
 use hyper::{Body, Method, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use once_cell::sync::Lazy;
+use tokio::sync::broadcast::error::RecvError::Lagged;
 use tokio::sync::mpsc;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
@@ -19,14 +24,15 @@ use assemblylift_core_iomod::package::IomodManifest;
 use assemblylift_core_iomod::registry;
 
 use crate::abi::GenericDockerAbi;
-use crate::runner::{RunnerMessage, RunnerTx, spawn_runner};
+use crate::runner::{Runner, RunnerMessage, RunnerTx};
 use crate::Status::{Failure, Success};
 
 mod abi;
 mod runner;
 
-pub type StatusTx = mpsc::Sender<Status>;
-pub type StatusRx = mpsc::Receiver<Status>;
+pub type StatusTx = crossbeam_channel::Sender<Status>;
+pub type StatusRx = crossbeam_channel::Receiver<Status>;
+pub type StatusChannel = (StatusTx, StatusRx);
 
 #[derive(Clone)]
 pub enum Status {
@@ -34,10 +40,50 @@ pub enum Status {
     Failure(String),
 }
 
-async fn launcher(
+pub struct Launcher {
+    channel: StatusChannel,
+    runtime: tokio::runtime::Runtime,
+}
+
+impl Launcher {
+    pub fn new() -> Self {
+        Self {
+            // channel: mpsc::channel::<Status>(32),
+            channel: bounded(32),
+            runtime: tokio::runtime::Runtime::new().unwrap(),
+        }
+    }
+
+    pub fn spawn(&mut self, runner_tx: RunnerTx) {
+        crossbeam_utils::thread::scope(|s| {
+            s.spawn(move |_| {
+                tokio::task::LocalSet::new().block_on(&self.runtime, async {
+                    let make_svc = make_service_fn(|_conn| {
+                        let runner_tx = runner_tx.clone();
+                        let tx = self.channel.0.clone();
+                        let rx = self.channel.1.clone();
+                        async {
+                            Ok::<_, Infallible>(service_fn(move |req| {
+                                launch(req, runner_tx.clone(), tx.clone(), rx.clone())
+                            }))
+                        }
+                    });
+
+                    let addr = SocketAddr::from(([0, 0, 0, 0], 5543));
+                    if let Err(e) = Server::bind(&addr).serve(make_svc).await {
+                        eprintln!("server error: {}", e);
+                    }
+                });
+            });
+        }).unwrap();
+    }
+}
+
+async fn launch(
     req: Request<Body>,
     runner_tx: RunnerTx,
-    mut status_rx: StatusRx,
+    status_tx: StatusTx,
+    status_rx: StatusRx,
 ) -> Result<Response<Body>, Infallible> {
     if req.method() != Method::POST {
         return Ok(Response::builder()
@@ -49,7 +95,9 @@ async fn launcher(
     let input_bytes = hyper::body::to_bytes(req.into_body()).await.unwrap();
 
     let msg = RunnerMessage {
+        // request_id: String,
         input: input_bytes.to_vec(),
+        status_sender: status_tx.clone(),
     };
     tokio::spawn(async move {
         if let Err(e) = runner_tx.send(msg).await {
@@ -57,7 +105,7 @@ async fn launcher(
         }
     });
 
-    if let Some(result) = status_rx.recv().await {
+    if let Ok(result) = status_rx.recv() {
         return Ok(match result {
             Success(_) => Response::builder()
                 .status(200)
@@ -76,8 +124,7 @@ async fn launcher(
         .unwrap())
 }
 
-#[tokio::main]
-pub async fn main() {
+fn main() {
     println!("Starting AssemblyLift generic runtime {}", crate_version!());
 
     let subscriber = FmtSubscriber::builder()
@@ -149,41 +196,41 @@ pub async fn main() {
         }
     }
 
+    let (status_tx, status_rx) = mpsc::channel::<Status>(32);
+    let (registry_tx, registry_rx) = mpsc::channel(32);
+    registry::spawn_registry(registry_rx).unwrap();
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 5543));
-
-    let make_svc = make_service_fn(|_conn| async {
-        let (runner_tx, runner_rx) = mpsc::channel(32);
-        let (status_tx, status_rx) = mpsc::channel::<Status>(32);
-        let (registry_tx, registry_rx) = mpsc::channel(32);
-        registry::spawn_registry(registry_rx).unwrap();
-
-        let (module, resolver, threader_env) =
-            match wasm::build_module_from_path::<GenericDockerAbi, Status>(
-                registry_tx,
-                status_tx.clone(),
-                "/opt/assemblylift", // TODO get from env
-                "handler",           // TODO get from env
-            ) {
-                Ok(module) => (Arc::new(module.0), module.1, module.2),
-                Err(_) => panic!("Unable to build WASM module"),
-            };
-
-        spawn_runner(
+    // TODO move to launcher after status channel is created
+    let (module, resolver, threader_env) =
+        match wasm::build_module_from_path::<GenericDockerAbi, Status>(
+            registry_tx,
             status_tx.clone(),
-            runner_rx,
-            module.clone(),
-            resolver.clone(),
-            threader_env.clone(),
-        );
+            "/opt/assemblylift", // TODO get from env
+            "handler",           // TODO get from env
+        ) {
+            Ok(module) => (Arc::new(module.0), module.1, module.2),
+            Err(_) => panic!("Unable to build WASM module"),
+        };
+    //----
 
-        let mut rx = Some(status_rx);
-        Ok::<_, Infallible>(service_fn(move |req| {
-            launcher(req, runner_tx.clone(), rx.take().unwrap())
-        }))
-    });
+    let mut runner = Runner::new();
+    runner.spawn(module, resolver, threader_env);
 
-    if let Err(e) = Server::bind(&addr).serve(make_svc).await {
-        eprintln!("server error: {}", e);
-    }
+    let mut launcher = Launcher::new();
+    launcher.spawn(runner.sender());
+
+    // let mut launcher = Arc::new(Launcher::new());
+    // TODO invert? move service into launcher
+    // let make_svc = make_service_fn(|_conn| async {
+    //     let runner = runner.clone();
+    //
+    //     Ok::<_, Infallible>(service_fn(move |req| {
+    //         launcher.clone().launch(req, runner.clone())
+    //     }))
+    // });
+    //
+    // let addr = SocketAddr::from(([0, 0, 0, 0], 5543));
+    // if let Err(e) = Server::bind(&addr).serve(make_svc).await {
+    //     eprintln!("server error: {}", e);
+    // }
 }
--- providers/generic-docker/src/runner.rs
+++ providers/generic-docker/src/runner.rs
@@ -12,36 +12,55 @@ use crate::{Status, StatusTx};
 
 pub type RunnerTx = mpsc::Sender<RunnerMessage>;
 pub type RunnerRx = mpsc::Receiver<RunnerMessage>;
+pub type RunnerChannel = (RunnerTx, RunnerRx);
 
 #[derive(Clone)]
 pub struct RunnerMessage {
     pub input: Vec<u8>,
+    pub status_sender: StatusTx,
 }
 
-pub fn spawn_runner(tx: StatusTx, mut rx: RunnerRx, module: Arc<Module>, resolver: Resolver, env: ThreaderEnv<Status>) {
-    std::thread::spawn(move || {
-        let mut rt = tokio::runtime::Runtime::new().unwrap();
-        tokio::task::LocalSet::new().block_on(&mut rt, async {
-            while let Some(msg) = rx.recv().await {
-                env.host_input_buffer
-                    .clone()
-                    .lock()
-                    .unwrap()
-                    .initialize(msg.input);
-                // TODO instance pooling
-                let instance = match wasm::new_instance(module.clone(), resolver.clone()) {
-                    Ok(instance) => Arc::new(instance),
-                    Err(why) => panic!("Unable to spin new WASM instance {}", why.to_string()),
-                };
-                let tx = tx.clone();
-                tokio::task::spawn_local(async move {
-                    let start = instance.exports.get_function("_start").unwrap();
-                    match start.call(&[]) {
-                        Ok(_) => tx.send(Status::Success("".to_string())).await,
-                        Err(_) => tx.send(Status::Failure("WASM module exited in error".to_string())).await,
+pub struct Runner {
+    channel: RunnerChannel,
+}
+
+impl Runner {
+    pub fn new() -> Self {
+        Runner {
+            channel: mpsc::channel(32), // TODO move out? or make crossbeam channel
+        }
+    }
+
+    pub fn spawn(&mut self, module: Arc<Module>, resolver: Resolver, env: ThreaderEnv<Status>) {
+        crossbeam_utils::thread::scope(|s| {
+            s.spawn(move |_| {
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                tokio::task::LocalSet::new().block_on(&rt, async {
+                    while let Some(msg) = self.channel.1.recv().await {
+                        env.host_input_buffer
+                            .clone()
+                            .lock()
+                            .unwrap()
+                            .initialize(msg.input);
+                        // TODO instance pooling
+                        let instance = match wasm::new_instance(module.clone(), resolver.clone()) {
+                            Ok(instance) => Arc::new(instance),
+                            Err(why) => panic!("Unable to spin new WASM instance {}", why.to_string()),
+                        };
+                        tokio::task::spawn_local(async move {
+                            let start = instance.exports.get_function("_start").unwrap();
+                            match start.call(&[]) {
+                                Ok(_) => msg.status_sender.send(Status::Success("".to_string())).unwrap(),
+                                Err(_) => msg.status_sender.send(Status::Failure("WASM module exited in error".to_string())).unwrap(),
+                            }
+                        });
                     }
                 });
-            }
-        });
-    });
+            });
+        }).unwrap()
+    }
+
+    pub fn sender(&self) -> RunnerTx {
+        self.channel.0.clone()
+    }
 }
