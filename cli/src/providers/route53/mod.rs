cbab718d0f9417bc99255cd24f0bdc13dbde4651
xlem <xlem@akkoro.io>
2022-08-20 12:38:24 -0230

    start r53 template & cast

--- cli/src/providers/route53/mod.rs
+++ cli/src/providers/route53/mod.rs
@@ -1,11 +1,15 @@
 use std::rc::Rc;
 use std::sync::Arc;
 
+use handlebars::Handlebars;
+use serde::Serialize;
+
 use crate::providers::{Options, Provider, ProviderError};
-use crate::transpiler::{Artifact, Bindable, Bootable, Castable, CastError};
+use crate::transpiler::{Artifact, Bindable, Bootable, Castable, CastError, ContentType, Template};
 use crate::transpiler::context::Context;
 
 pub struct DnsProvider {
+    /// access_key_id, secret_key, aws_region, hosted_zone_id
     options: Arc<Options>,
 }
 
@@ -18,37 +22,69 @@ impl DnsProvider {
 }
 
 impl Castable for DnsProvider {
-    fn cast(&self, ctx: Rc<Context>, selector: Option<&str>) -> Result<Vec<Artifact>, CastError> {
-        todo!()
+    fn cast(&self, ctx: Rc<Context>, _selector: Option<&str>) -> Result<Vec<Artifact>, CastError> {
+        let project_name = ctx.project.name.clone();
+        // let domains = ctx.domains;
+
+        let rendered_hcl = Route53Template {
+            project_name,
+            hosted_zone_id: "".to_string(),
+        }.render();
+        let out = Artifact {
+            content_type: ContentType::HCL("HCL"),
+            content: rendered_hcl,
+            write_path: "net/plan.tf".into(),
+        };
+        Ok(vec![out])
     }
 }
 
 impl Bindable for DnsProvider {
     fn bind(&self, ctx: Rc<Context>) -> Result<(), CastError> {
-        todo!()
+        Ok(())
     }
 }
 
 impl Bootable for DnsProvider {
     fn boot(&self, ctx: Rc<Context>) -> Result<(), CastError> {
-        todo!()
+        Ok(())
     }
 
     fn is_booted(&self, ctx: Rc<Context>) -> bool {
-        todo!()
+        true
     }
 }
 
 impl Provider for DnsProvider {
     fn name(&self) -> String {
-        todo!()
+        String::from("route53")
     }
 
     fn options(&self) -> Arc<Options> {
-        todo!()
+        self.options.clone()
     }
 
     fn set_options(&mut self, opts: Arc<Options>) -> Result<(), ProviderError> {
-        todo!()
+        self.options = opts;
+        Ok(())
+    }
+}
+
+#[derive(Serialize)]
+struct Route53Template {
+    project_name: String,
+    hosted_zone_id: String,
+}
+
+impl Template for Route53Template {
+    fn render(&self) -> String {
+        let mut reg = Box::new(Handlebars::new());
+        reg.register_template_string("tmpl", Self::tmpl()).unwrap();
+        reg.render("tmpl", &self).unwrap()
+    }
+
+    fn tmpl() -> &'static str {
+        r#"#Begin Route53
+        "#
     }
 }
