21fd3e85f7f8c9767c8a0f4d1f2572ea7d62a615
xlem <xlem@akkoro.io>
2022-09-05 20:04:52 -0230

    fix apigw domain mapping

--- cli/src/providers/aws_lambda.rs
+++ cli/src/providers/aws_lambda.rs
@@ -533,22 +533,7 @@ resource aws_apigatewayv2_stage {{service_name}}_default_stage {
     name        = "$default"
     auto_deploy = true
 }
-
-{{#if has_domain_name}}resource aws_apigatewayv2_domain_name {{service_name}}_domain {
-  domain_name = "{{service_name}}.{{domain_name}}"
-
-  domain_name_configuration {
-    certificate_arn = "{{options.certificate_arn}}"
-    endpoint_type   = "REGIONAL"
-    security_policy = "TLS_1_2"
-  }
-}
-
-resource aws_apigatewayv2_api_mapping {{service_name}} {
-  api_id      = aws_apigatewayv2_api.{{service_name}}_http_api.id
-  domain_name = aws_apigatewayv2_domain_name.{{service_name}}_domain.id
-  stage       = "$default"
-}{{/if}}{{/if}}
+{{/if}}
 
 {{#each authorizers}}resource aws_apigatewayv2_authorizer {{../service_name}}_{{this.id}} {
     provider    = aws.{{../project_name}}-aws-lambda
--- cli/src/providers/route53/mod.rs
+++ cli/src/providers/route53/mod.rs
@@ -205,6 +205,12 @@ resource aws_acm_certificate_validation {{this.name}} {
   certificate_arn         = aws_acm_certificate.{{this.name}}.arn
   validation_record_fqdns = [for record in aws_route53_record.{{this.name}}_validation : record.fqdn]
 }
+
+resource aws_apigatewayv2_api_mapping {{this.name}} {
+  api_id      = aws_apigatewayv2_api.{{this.name}}_http_api.id
+  domain_name = aws_apigatewayv2_domain_name.{{this.name}}_domain.id
+  stage       = "$default"
+}
 {{/if}}
 resource aws_route53_record {{this.name}} {
   provider = aws.{{../../project_name}}-r53-{{../name_snaked}}
